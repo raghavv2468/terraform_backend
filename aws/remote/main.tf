e75a12f5c4297f9a361264bd820fc662409cd0c8
raghavv2468 <106361352+raghavv2468@users.noreply.github.com>
2024-11-30 18:59:51 +0530

    Update main.tf

--- aws/remote/main.tf
+++ aws/remote/main.tf
@@ -1,1 +1,17 @@
+rovider "aws" {
+  region     = "ap-south-1"
+  
+}
+
+resource "aws_instance" "Demo" {
+  ami                    = "ami-022ce6f32988af5fa"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-05f5fc31ced5b1d9f"]                                                        
+
+  key_name               = "manishi"
+  tags = {
+      Name = "Redhat Server by Terraform"
+    }
+
+}
 
