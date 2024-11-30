33b902235b677111e22a6572c5a0731335cdaebf
raghavv2468 <106361352+raghavv2468@users.noreply.github.com>
2024-11-30 19:24:27 +0530

    Update main.tf

--- aws/remote/main.tf
+++ aws/remote/main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "my-raghava-bucket-"  # Replace with a unique bucket name
+  bucket = "my-raghava-bucket-123"  # Replace with a unique bucket name
   acl    = "private"  # You can change this to other ACLs like 'public-read', etc.
 
 
 resource "aws_dynamodb_table" "example_table" {
-  name           = "ramuu-table"  # DynamoDB table name
+  name           = "ramuu-456-table"  # DynamoDB table name
   billing_mode   = "PROVISIONED"    # You can also use "PAY_PER_REQUEST" for on-demand billing
   hash_key       = "raghava_raghu"             # Partition key
   read_capacity  = 5                # Read capacity units (only for PROVISIONED mode)
@@ -18,6 +18,18 @@ resource "aws_dynamodb_table" "example_table" {
     name = "raghava_raghu"                     # Attribute name for the hash key
     type = "S"                      # Attribute type, 'S' is for string
   }
+
+terraform {
+  backend "s3" {
+    bucket         = "my-raghava-bucket-123"  # The name of your S3 bucket
+    key            = "terraform.tfstate"  # The name of your state file in S3
+    region         = "us-east-1"          # The AWS region
+    encrypt        = true                  # Enable encryption for the state file
+    dynamodb_table = "ramuu-456-table"  # The DynamoDB table for state locking
+    acl            = "private"  # Set access control list for the state file
+  }
+}
+
 
 
 
