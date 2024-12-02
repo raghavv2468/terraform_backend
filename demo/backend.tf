8e748442b83c2a2d3b50e0337637e60418507f32
raghavv2468 <106361352+raghavv2468@users.noreply.github.com>
2024-12-02 13:24:15 +0530

    Update backend.tf

--- demo/backend.tf
+++ demo/backend.tf
@@ -1,1 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "raghu-technology-190"  
+    key            = "env:/terraform.tfstate" 
+    region         = "us-east-1"          # AWS region
+    encrypt        = true                  # Enable encryption for state file
+    dynamodb_table = "raghava-table"  # DynamoDB table for locking
+    acl            = "private"  # Access control for state file
+  }
+}
 
